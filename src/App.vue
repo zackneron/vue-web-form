6a7331cb37e9fdec20b201ff56e7c4d2472bcec3
Katshi Mutombo <katshi.mutombo@sanlam.co.za>
2021-12-29 10:50:31 +0200

    Init project and clean up default content

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+body {
+  margin: 0;
+  background-color: #eee;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>

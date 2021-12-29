d96969b5e762586791141e215c087918898f4c03
Katshi Mutombo <katshi.mutombo@sanlam.co.za>
2021-12-29 11:16:38 +0200

    sign up form init and add fields

---
+++ src/components/SignupForm.vue
@@ -0,0 +1,47 @@
+<template>
+  <form>
+      <label>Email:</label>
+      <input type="email" required v-model='email'>
+      <label>Password:</label>
+      <input type="password" required v-model='password'>
+  </form>
+  <p>{{email}} - {{password}}</p>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            email: '',
+            password: ''
+        }
+    }
+}
+</script>
+
+<style>
+form {
+    max-width: 420px;
+    margin: 30px auto;
+    background-color: white;
+    text-align: left;
+    padding: 40px;
+    border-radius: 10px;
+}
+label {
+    color: #aaa;
+    display: inline-block;
+    margin: 25px 0 15px;
+    text-transform: uppercase;
+    font-weight: bold;
+}
+input {
+    display: block;
+    padding: 10px 6px;
+    width: 100%;
+    box-sizing: border-box;
+    border: none;
+    border-bottom: 1px solid #ddd;
+    color: #555;
+}
+</style>
